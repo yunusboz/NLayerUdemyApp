fc580384655e90ef6375fe8abb3b63702e9d51e3
yunus.bozkurt <yunus.bozkurt@NB014-PAYGO>
2022-05-14 15:50:02 +0300

    Add project files.

---
+++ NLayer.API/Modules/RepoServiceModule.cs
@@ -0,0 +1,43 @@
+﻿using Autofac;
+using NLayer.Repository;
+using NLayer.Service.Mapping;
+using System.Reflection;
+using NLayer.Repository.Repositories;
+using NLayer.Core.Repositories;
+using NLayer.Service.Service;
+using NLayer.Core.Services;
+using NLayer.Repository.UnitOfWorks;
+using NLayer.Core.UnitOfWorks;
+using Module = Autofac.Module;
+using NLayer.Caching;
+
+namespace NLayer.API.Modules
+{
+    public class RepoServiceModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+
+            builder.RegisterGeneric(typeof(GenericRepository<>)).As(typeof(IGenericRepository<>)).InstancePerLifetimeScope();
+            builder.RegisterGeneric(typeof(Service<>)).As(typeof(IService<>)).InstancePerLifetimeScope();
+
+            builder.RegisterType<UnitOfWork>().As<IUnitOfWork>();
+
+
+
+
+            var apiAssembly = Assembly.GetExecutingAssembly();
+            var repoAssembly = Assembly.GetAssembly(typeof(AppDbContext));
+            var serviceAssembly = Assembly.GetAssembly(typeof(MapProfile));
+
+            builder.RegisterAssemblyTypes(apiAssembly, repoAssembly, serviceAssembly).Where(x => x.Name.EndsWith("Repository"))
+                .AsImplementedInterfaces().InstancePerLifetimeScope();
+
+            builder.RegisterAssemblyTypes(apiAssembly, repoAssembly, serviceAssembly).Where(x => x.Name.EndsWith("Service"))
+                .AsImplementedInterfaces().InstancePerLifetimeScope();
+
+            builder.RegisterType<ProductServiceWithCaching>().As<IProductService>();
+
+        }
+    }
+}
